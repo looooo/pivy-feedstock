a683ebc5eb7759469f1b9711f8a510bee0120a0b
looooo <sppedflyer@gmail.com>
2019-10-15 17:33:05 +0200

    remove some qt-config lines with sed

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,8 @@
+if [[ ${HOST} =~ .*linux.* ]]; then
+  sed -i 's|_qt5gui_find_extra_libs(EGL.*)|_qt5gui_find_extra_libs(EGL "EGL" "" "")|g' $PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
+  sed -i 's|_qt5gui_find_extra_libs(OPENGL.*)|_qt5gui_find_extra_libs(OPENGL "GL" "" "")|g' $PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
+fi
+
 PIVY_CPP_FLAGS='-std=c++1z '
 
 ${PYTHON} setup.py clean
